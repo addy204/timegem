9a6c321db37ef86fbfd3859adf51c205ef578262
addy204 <aksharma@academic.rrc.ca>
2024-07-24 07:46:47 -0500

    Made Adjustment too  sign-up and sign-in functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_cart
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper OrdersHelper
 
   private
 
   def initialize_cart
     @cart = session[:cart] ||= { 'items' => [] }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
   end
 end
