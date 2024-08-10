9a1a054741f738e357f72c0db0cb865fd789a98a
addy204 <aksharma@academic.rrc.ca>
2024-08-10 10:34:43 -0500

    validated html

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,4 @@ import { definitionsFromContext } from "@hotwired/stimulus-loading";
 const application = Application.start();
 const context = require.context("controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
+import "stylesheets/application";
